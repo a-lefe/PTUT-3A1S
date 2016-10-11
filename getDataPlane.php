2130032a8b501f4dd2fcf88f0213aa97724656b4
baptistegu <guegan.baptiste@gmail.com>
2016-10-11 16:50:08 +0200

    Adaptation lancement cron sur alwaysdata

--- getDataPlane.php
+++ getDataPlane.php
@@ -1,5 +1,4 @@
 <?php
 $file = file_get_contents('https://alexandre.lefebvre@etu.univ-lyon1.fr:polytech3a@download.data.grandlyon.com/ws/rdata/adl_aeroport_lyon.adlvoloperationneldepart/all.json');
-$local_file = fopen('all_data.json', 'w+');
+$local_file = fopen('www/all_data.json', 'w+');
 fputs($local_file, $file);
-echo true;
