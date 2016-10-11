86cfb492ee686ac9e71043f83dd7f5a55f8ee535
Jérôme Alibert <jeromealibert96@gmail.com>
2016-10-11 16:46:41 +0200

    Récupération donnée du tableau

--- connect_mysql.php
+++ connect_mysql.php
@@ -5,6 +5,18 @@ $databases = array(
                 'login'     => '128339',
                 'password'  => 'polytech3a',
             );
+$queryToExecute = POST['queryToExecute'];
+$result = null;
 $pdo = new PDO('mysql:host='.$databases['host'].';dbname='.$databases['database'].'; charset=utf8', $databases['login'], $databases['password']);
-return $pdo;
-?>
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+switch($queryToExecute){
+    case "indexArray":
+        $sql = $pdo->prepare("SELECT flightnumbers_tripnumber, airports_destination_name, airlines_airline_name, timestamps_sobt FROM plane");
+        $sql->execute();
+        $result = $sql->fetch();
+        break;
+}
+if(!is_array($result))
+    $result = array("message" => "Une erreur est survenue lors de la connection à la base...", "success" => false);
+
+echo json_encode($result);
