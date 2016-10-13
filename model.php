da8383daf26b6d62a8be85854436950be101b7b6
baptistegu <guegan.baptiste@gmail.com>
2016-10-13 16:16:47 +0200

    modif requête + connexion bdd : localhost

--- model.php
+++ model.php
@@ -4,14 +4,14 @@ header('Cache-Control: no-cache, must-revalidate');
 header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 header('Content-type: application/json');
 
-$pdo = require_once('connect_mysql.php');
-/*$pdo = require_once('connect_localhost.php');*/
+//$pdo = require_once('connect_mysql.php');
+$pdo = require_once('connect_localhost.php');
 $queryToExecute = $_POST['queryToExecute'];
 $result = null;
 
 switch($queryToExecute){
     case "indexArray":
-        $sql = $pdo->prepare("SELECT flightnumbers_tripnumber, airports_destination_name, airlines_airline_name, timestamps_sobt FROM plane ORDER BY timestamps_sobt DESC");
+        $sql = $pdo->prepare("SELECT gid, flightnumbers_tripnumber, airports_destination_name, airlines_airline_name, timestamps_sobt FROM plane ORDER BY timestamps_sobt DESC");
         $sql->execute();
         $result = $sql->fetchAll();
         break;
